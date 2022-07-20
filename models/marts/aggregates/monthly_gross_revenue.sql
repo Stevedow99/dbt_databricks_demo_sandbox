0463e7de58d5e8ada8d932dc066c32f5e07bba5b
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-07-20 20:45:29 +0000

    updates

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,12 +1,9 @@
 select
-    region_name,
     date_trunc(month, order_date) as order_month,
     sum(gross_item_sales_amount) as gross_revenue
 
 from {{ ref('fct_order_items') }}
     group by 
-        region_name,
         order_month
     order by 
-        region_name,
         order_month
