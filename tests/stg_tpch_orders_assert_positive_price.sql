eee91f26f021455d9c94fc33362101d90f18b05f
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-07-20 00:59:23 +0000

    tests

--- tests/stg_tpch_orders_assert_positive_price.sql
+++ tests/stg_tpch_orders_assert_positive_price.sql
@@ -1,13 +1,4 @@
-{{
-    config(
-        enabled=true,
-        severity='error',
-        tags = ['finance']
-    )
-}}
-
-with orders as ( select * from {{ ref('stg_tpch_orders') }} )
-
-select *
-from   orders 
-where  total_price < 0
+select 
+	*
+from {{ ref('stg_tpch_orders') }}
+	where total_price < 0
