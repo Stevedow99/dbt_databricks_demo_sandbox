4b5b69af6522e1db355c1b976dcd07436c1f8611
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-07-21 12:55:21 +0000

    updates

--- models/samples/my_first_model.sql
+++ models/samples/my_first_model.sql
@@ -3,5 +3,5 @@
 
 Select 
     'this is great!' as my_frist_column,
-    1/11 as a_simple_calculation,
+    1/1 as a_simple_calculation,
     current_timestamp() as current_time
---
+++ models/samples/my_second_model.sql
@@ -0,0 +1,5 @@
+
+
+Select 
+    *
+from {{ ref('my_first_model') }}
