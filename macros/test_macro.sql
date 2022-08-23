2e3af86ebc5053c76bf988ad1d9576a4c106efe1
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-08-23 16:29:20 +0000

    commit

--- macros/test_macro.sql
+++ macros/test_macro.sql
@@ -1,15 +1,26 @@
 {% macro test_macro() -%}
 
-    {% if execute %}
+{#########################################################################}
+{# checks what command was run, and does something different based on it #}
+{#########################################################################}
+
+    {% if flags.WHICH == 'compile' %}
+
+        {{ log("dbt compile called therefore doing this command", info=True) }}
 
         {% do run_query("SELECT 1") %}
 
+    {% elif flags.WHICH == 'run' %}
+
+        {{ log("dbt run called therefore doing this command", info=True) }}
+
+        {% do run_query("SELECT 2") %}
+
     {% else %}
 
-         {% do run_query("SELECT 2") %}
+        {{ log("compile or run was not called, not doing anything", info=True) }}
+
 
     {% endif %}
-
-    {% do run_query("SELECT 3") %}
 
 {%- endmacro %}
--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,3 +1,7 @@
+{{
+config(  pre_hook = test_macro()) 
+}}
+
 select
     date_trunc('MM', order_date) as order_month,
     sum(gross_item_sales_amount) as gross_revenue
